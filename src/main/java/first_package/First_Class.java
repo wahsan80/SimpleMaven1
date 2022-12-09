1ff51ff724d461b2613441ed9504ee16863f20ea
wahsan80 <w_ahsan@hotmail.com>
2022-12-09 02:03:00 -0500

    Simple Maven 1 Project

---
+++ src/main/java/first_package/First_Class.java
@@ -0,0 +1,11 @@
+package first_package;
+
+public class First_Class {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is the first class of first package of first Maven project.");
+
+	}
+
+}
